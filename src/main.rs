cc653d08ffacc26fa59abc1aa4f01d591d193295
snake <admin@viasnake.com>
2021-11-18 12:56:30 +0900

    mut

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ fn main() {
     // コメント
     println!("Hello Rust!");
 
-    let x = 10;
+    let mut x = 10;
     let y = 5.55;
     println!("x:i32 = {}, y:i64 = {}",x ,y);
+
+    x = 20;
+    println!("x is mutable: x = {}", x);
 }
