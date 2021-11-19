6f26408de4a75d602b3619c74231908041c621ea
snake <admin@viasnake.com>
2021-11-19 09:11:14 +0900

    cal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+const PI_F32: f32 = 3.141592653589793;
+
 fn main() {
     // コメント
     println!("Hello Rust!");
@@ -13,6 +15,16 @@ fn main() {
     let t = (13, false);
     let s = "hello world!";
     println!("{} {} {} {}", b, t.0, t.1, s);
-
     println!("明示的な型変換: x + y = {}", x as f64 + y);
+    println!("CONST PI_F32 = {}", PI_F32);
+
+    let nums: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", nums);
+
+    let result = cal(30,64);
+    println!("30 + 64 = {} , 30 * 64 = {}", result.0, result.1);
+}
+
+fn cal(x: i32, y: i32) -> (i32, i32) {
+    return (x + y, x * y);
 }
